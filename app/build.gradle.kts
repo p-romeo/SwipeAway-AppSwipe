2d46a10b69c29127c95baa9a5aa6413c1416b3dc
Paul Romeo <13206466+p-romeo@users.noreply.github.com>
2026-05-22 20:38:45 -0400

    build completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.appswipe.axbycz"
     minSdk = 24
     targetSdk = 36
-    versionCode = 6
-    versionName = "6.0"
+    versionCode = 7
+    versionName = "7.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
